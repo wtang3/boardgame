9e87ae2a3798cf5ba544fb848a99876648bc3652
William Tang <wtang3@outlook.com>
2016-05-01 19:08:19 -0400

    added template inheritance

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,5 +1,13 @@
 from django.conf.urls import patterns, include, url
 
-urlpatterns = patterns('profiles.views',
-    url(r'^home$', 'home', name='profiles_home')
-)
+#urlpatterns = patterns('profiles.views',
+#    url(r'^home$', 'home', name='profiles_home')
+#)
+
+import profiles.views
+import main.views
+
+urlpatterns = [
+    url(r'^$', profiles.views.home,
+    name='profiles_home'),
+]
