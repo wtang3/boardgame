8cbfbe659f76544f6fcb369937c79b7393312649
icebooda <wtang3.outlook.com>
2016-04-30 23:27:16 -0400

    modified models

--- tictactoe/models.py
+++ tictactoe/models.py
@@ -3,6 +3,13 @@ from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
 
+GAME_STATUS_CHOICES = (
+    ('A', 'Active'),
+    ('F', 'First player wins'),
+    ('S', 'Second player wins'),
+    ('D', 'Draw')
+)
+
 # Create your models here.
 class Game(models.Model):
     first_player = models.ForeignKey(User, related_name="games_first_player")
@@ -10,6 +17,10 @@ class Game(models.Model):
     next_to_move = models.ForeignKey(User, related_name="games_to_move")
     start_time = models.DateTimeField(auto_now=True)
     last_active = models.DateTimeField(auto_now=True)
+    status  = models.CharField(max_length=1, default='A', choices=GAME_STATUS_CHOICES)
+
+    def __str__(self):
+        return "{0} vs {1}".format(self.first_player, self.second_player)
 
 class Move(models.Model):
     x = models.IntegerField()
